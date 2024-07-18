5e768b77689eff60e8341104473c36aec727ed41
Daniel Crigan <crgdaniel@yahoo.com>
2024-07-18 23:44:26 +0100

    add naming convention for models

--- fastapi-application/core/models/__init__.py
+++ fastapi-application/core/models/__init__.py
@@ -1,9 +1,11 @@
 __all__ = (
     'db_helper',
-    'Base'
+    'Base',
+    'User'
 
 )
 
 
 from .db_helper import db_helper
 from .base import Base
+from .user import User
---
+++ fastapi-application/core/models/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import UniqueConstraint
+from .base import Base
+
+
+class User(Base):
+    username: Mapped[str] = mapped_column(unique=True)
+    foo: Mapped[int]
+    bar: Mapped[int]
+
+    __table_args__ = (
+        UniqueConstraint("foo", "bar"),
+    )
