3f404fd4e9d0712512db6291614ca8667ed51c4b
Daniel Crigan <crgdaniel@yahoo.com>
2024-07-18 21:03:57 +0100

    add db helper dispose to main app

--- fastapi-application/core/models/db_helper.py
+++ fastapi-application/core/models/db_helper.py
@@ -33,6 +33,7 @@ class DatabaseHelper:
     async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
         async with self.session_factory() as session:
             yield session
+            
 
 db_helper = DatabaseHelper(
     url=str(settings.db.url),
