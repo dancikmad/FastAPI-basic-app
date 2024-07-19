5b766dce21be26fb775c26bcb5b329a6d14862a7
Daniel Crigan <crgdaniel@yahoo.com>
2024-07-19 19:54:36 +0100

    custom static urls for docs

--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -1,27 +1,19 @@
-from contextlib import asynccontextmanager
 import uvicorn
-from fastapi import FastAPI
 
 from core.config import settings
+
 from api import router as api_router
-from core.models import db_helper
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # startup
-    yield
-    # shutdown
-    print("dispose engine")
-    await db_helper.dispose()
+from create_fastapi_app import create_app
 
 
 
-main_app = FastAPI(
-    lifespan=lifespan,
+
+main_app = create_app(
+    create_custom_static_urls=True,
 )
+
 main_app.include_router(
     api_router,
-    prefix=settings.api.prefix,
 )
 
 if __name__ == "__main__":
